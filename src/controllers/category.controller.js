7c8118fe17595ac6f3ccadb211bd77b446625f2e
Denisexper <clasic.exper2023@gmail.com>
2025-11-05 08:59:17 -0600

    feat: finish endpoints categories modules

--- src/controllers/category.controller.js
+++ src/controllers/category.controller.js
@@ -30,4 +30,126 @@ export class categoryController {
 
 
     }
+
+    async getCategory (req, res) {
+
+        const { id } = req. params;
+
+        try {
+            
+            const response = await prisma.category.findFirst({
+                where: {
+                    id: Number(id)
+                },
+                include:{
+                    products: true
+                }
+            })
+
+            if(!response){
+                return res.status(404).json({
+                    msj: "category not found",
+                    
+                })
+            }
+
+            res.status(200).json({
+                msj: "category",
+                data: response
+            })
+
+        } catch (error) {
+            
+             res.status(500).json({
+                msj: "uknow error",
+                error: error.message
+             })
+        }
+    }
+
+    async createCategory (req, res) {
+
+        try {
+            
+            const response = await prisma.category.create({
+
+                data: req.body,
+            })
+
+            res.status(200).json({
+                msj: "category created",
+                data: response
+            })
+        } catch (error) {
+            
+            res.status(500).json({
+                msj: "uknow error",
+                error: error.message
+            })
+        }
+    }
+
+    async updateCategory (req, res) {
+
+        const { id } = req.params;
+
+        try {
+            
+            const response = await prisma.category.update({
+                where:{
+                    id: Number(id)
+                },
+                data: req.body,
+            })
+
+            res.status(200).json({
+                msj: "category updated",
+                data: response
+            })
+        } catch (error) {
+            
+            if(error.code === "P2025"){
+                return res.status(404).json({
+                    msj: "category not found"
+                })
+            }
+
+            res.status(500).json({
+                msj: "uknow error",
+                error: error.message
+            })
+        }
+    }
+
+    async deleteCategory (req, res) {
+
+        const { id } = req.params;
+
+        try {
+            
+            const response = await prisma.category.delete({
+                where: {
+                    id: Number(id)
+                }
+            })
+
+            res.status(200).json({
+                msj: "category deleted",
+                data: response
+            })
+        } catch (error) {
+            
+            if(error.code === "P2025"){
+
+                return res.status(404).json({
+                    msj: "category not found"
+                })
+            }
+
+            res.status(500).json({
+                msj: "server error",
+                error: error.message
+            })
+        }
+    }
 }
--- src/routes/category.routes.js
+++ src/routes/category.routes.js
@@ -6,5 +6,9 @@ const controller = new categoryController()
 const router = Router()
 
 router.get("/get-all", controller.getAll)
+router.get("/get-category/:id", controller.getCategory)
+router.post("/create", controller.createCategory)
+router.put("/update-category/:id", controller.updateCategory)
+router.delete("/delete-category/:id", controller.deleteCategory)
 
 export default router
